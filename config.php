d71ad3984273cb64e0f3234aedde442bb7fb4b1f
TheXmanz <52388234+BangDanz@users.noreply.github.com>
2020-05-22 01:22:03 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+$ua = array(
+"Content-Type: application/x-www-form-urlencoded; charset=UTF-8",
+"User-Agent: Dalvik/2.1.0 (Linux; U; Android 9; SM-A107F Build/PPR1.180610.011)",
+"Host: tbrgamezone.com",
+"Connection: Keep-Alive",
+"Accept-Encoding: gzip",
+"Cookie: ---");
+
+$data1 = "";
+
+
+$head = array(
+  "Content-Type: application/x-www-form-urlencoded; charset=UTF-8",
+  "User-Agent: Dalvik/2.1.0 (Linux; U; Android 9; SM-A107F Build/PPR1.180610.011)",
+  "Host: tbrgamezone.com",
+  "Connection: Keep-Alive",
+  "Accept-Encoding: gzip",
+  "Cookie: ---");
+
+$data2 = "";
+
+?>
